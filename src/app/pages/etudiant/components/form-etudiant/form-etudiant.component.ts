1fc9b5285b787dcc4b4ba57cc939a7902531255d
OsirisJr16 <juniorhero81@gmail.com>
2024-01-22 21:57:23 +0300

    creat Read modify

--- src/app/pages/etudiant/components/form-etudiant/form-etudiant.component.ts
+++ src/app/pages/etudiant/components/form-etudiant/form-etudiant.component.ts
@@ -1,14 +1,14 @@
 import { Component ,OnInit,Input } from '@angular/core';
 import { NgbModal ,NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import { FormBuilder, FormGroup, Validators ,AbstractControl} from '@angular/forms';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-form-etudiant',
   templateUrl: './form-etudiant.component.html',
   styleUrls: ['./form-etudiant.component.css']
 })
 export class FormEtudiantComponent implements OnInit{
-    constructor(private formBuilder:FormBuilder,private modal : NgbActiveModal){}
+    constructor(private formBuilder:FormBuilder,private modal : NgbActiveModal , private location : Location){}
     formEtudiant !: FormGroup ;
     @Input() editMod:boolean =  false ; 
     @Input() studentData : any = {} ;
@@ -21,8 +21,23 @@ export class FormEtudiantComponent implements OnInit{
         const etudiants = JSON.parse(localStorage.getItem('etudiants') || '[]');
         etudiants.push(etudiant);
         localStorage.setItem('etudiants', JSON.stringify(etudiants));
+        window.location.reload() ; 
       }
     }
+    modifyEtudiantInLocalStorage(updatedEtudiant: any): void {
+      if (this.formEtudiant.valid) {
+        const etudiants = JSON.parse(localStorage.getItem('etudiants') || '[]');
+        const index = etudiants.findIndex((student: any) => student.id === updatedEtudiant.id);
+        if (index) {
+          etudiants[index] = updatedEtudiant;
+          localStorage.setItem('etudiants', JSON.stringify(etudiants));
+          window.location.reload()
+        } else {
+          console.error('student not found');
+        }
+      }
+    }
+    
     generateId(): string {
       return Math.random().toString(36).substr(2, 9);
     }
